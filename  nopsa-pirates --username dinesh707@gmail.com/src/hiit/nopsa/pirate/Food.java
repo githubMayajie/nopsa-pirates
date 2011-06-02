9b287157d02bedab365236da3e39f993880a8e89
dinesh707 <dinesh707@gmail.com>
2011-06-02 12:46:36 +0000

    Food Class contains parameters related to food

---
+++  nopsa-pirates --username dinesh707@gmail.com/src/hiit/nopsa/pirate/Food.java
@@ -0,0 +1,34 @@
+package hiit.nopsa.pirate;
+
+public class Food {
+	
+	private String icon_url; 		// http://nopsa.hiit.fi/viewer/images/square_2675942790_32794abcf1_t.jpg
+	private String tag;				// wine
+	private int score;				// 12  -  When player marks the bonderies he gets a score for item
+	private int last_img_marked;	// 16  -  Will be used to find what image was marked last
+	
+	public String getIcon_url() {
+		return icon_url;
+	}
+	public void setIcon_url(String icon_url) {
+		this.icon_url = icon_url;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public int getLast_img_marked() {
+		return last_img_marked;
+	}
+	public void setLast_img_marked(int last_img_marked) {
+		this.last_img_marked = last_img_marked;
+	}	
+}
